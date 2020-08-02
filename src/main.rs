150689b5117f5a926f01338e240674df057910e4
Arkadiusz Żmudzin <arek@moodup.team>
2020-08-02 21:05:16 +0200

    List all entries

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,20 @@ mod harvest;
 async fn main() -> Result<(), Box<dyn Error>> {
   let mut harvest = harvest::HarvestClient::new();
   harvest.prepare().await?;
-  println!("{:?}", harvest.time_entries().await);
+  let entries = harvest.time_entries().await?.time_entries;
+  println!("{:?}", entries);
+
+  entries.iter().for_each(|e| {
+    println!(
+      "{when},{duration},{what},{project}",
+      when = e.spent_date,
+      duration = e.hours,
+      what = format!("{} - {}", e.task.name, e.notes),
+      project = e.project.name
+    )
+  });
+
+  println!("{}", entries.iter().map(|e| e.hours).sum::<f64>());
 
   Ok(())
 }
