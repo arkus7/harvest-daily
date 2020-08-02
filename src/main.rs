15781f58a7a4d0b83b9edd279cbfeab9d993e40e
Arkadiusz Żmudzin <arek@moodup.team>
2020-08-02 19:19:20 +0200

    Update main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+mod harvest;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+  let mut harvest = harvest::HarvestClient::new();
+  harvest.prepare().await?;
+  println!("{:?}", harvest.time_entries().await);
+
+  Ok(())
 }
