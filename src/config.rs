a0556b4bebb9a6dc9f6d3e1f37fda614bd30bee7
Arkadiusz Żmudzin <arek@moodup.team>
2020-08-03 00:56:31 +0200

    Add handling config from command line

--- src/config.rs
+++ src/config.rs
@@ -1,25 +1,94 @@
+use chrono::{Local, NaiveDate};
 use serde::{Deserialize, Serialize};
-use std::path::{Path, PathBuf};
 
-#[derive(Debug, Serialize, Deserialize)]
+use std::{
+  fmt,
+  path::{Path, PathBuf},
+};
+use structopt::StructOpt;
+
+#[derive(Debug)]
+enum ConfigError {
+  TemplateNotFound { path: PathBuf },
+  InvalidDate { variable: String, value: String },
+}
+
+impl fmt::Display for ConfigError {
+  fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+    match self {
+      ConfigError::TemplateNotFound { path } => write!(
+        fmt,
+        "Template not found, path was: {path}",
+        path = std::fs::canonicalize(path).unwrap().to_str().unwrap()
+      ),
+      ConfigError::InvalidDate { variable, value } => write!(
+        fmt,
+        "Could not parse date, config variable: {var}, value: {value}",
+        var = variable,
+        value = value
+      ),
+    }
+  }
+}
+
+#[derive(Debug, Serialize, Deserialize, StructOpt)]
+#[structopt(name = "harvest-daily")]
 pub struct Config {
+  #[structopt(short, long = "template", parse(from_os_str))]
   pub template_path: Option<PathBuf>,
-  pub date_from: Option<String>,
-  pub date_to: Option<String>,
+  #[structopt(short, long = "start")]
+  pub start_date: Option<NaiveDate>,
+  #[structopt(short, long = "end")]
+  pub end_date: Option<NaiveDate>,
 }
 
 impl Default for Config {
   fn default() -> Self {
     Config {
       template_path: None,
-      date_from: None,
-      date_to: None,
+      start_date: Some(Local::today().naive_local()),
+      end_date: Some(Local::today().naive_local()),
     }
   }
 }
 
 impl Config {
-  pub fn from_toml(path: impl AsRef<Path>) -> Result<Config, confy::ConfyError> {
+  pub fn new() -> Result<Config, confy::ConfyError> {
+    let config = Config::from_toml("settings.toml").unwrap();
+    let args = Config::from_args();
+
+    Ok(Config::merge(vec![config, args]))
+  }
+
+  fn from_toml(path: impl AsRef<Path>) -> Result<Config, confy::ConfyError> {
     confy::load_path(path)
   }
+
+  fn merge(configs: Vec<Config>) -> Config {
+    let mut config = Config::default();
+    configs.iter().for_each(|c| {
+      if let Some(_) = c.template_path {
+        config.template_path = c.template_path.clone();
+      }
+      if let Some(_) = c.start_date {
+        config.start_date = c.start_date.clone();
+      }
+
+      if let Some(_) = c.end_date {
+        config.end_date = c.end_date.clone();
+      }
+    });
+    return config;
+  }
+
+  // fn validate(&self) -> Result<(), ConfigError> {
+  //   if let Some(template_path) = &self.template_path {
+  //     if let Err(err) = absolute_path(&template_path) {}
+  //   }
+  //   Ok(())
+  // }
 }
+
+// fn absolute_path(src: &PathBuf) -> Result<PathBuf, io::Error> {
+//   fs::canonicalize(src).map(|path| path)
+// }
