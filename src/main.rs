8612e6b3ffc80ac8187eb2739f5e8d3a5b39fa5b
Arkadiusz Żmudzin <arek@moodup.team>
2020-08-03 03:24:54 +0200

    Test displaying templated results

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
 use std::error::Error;
 
+mod config;
 mod harvest;
+mod template;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-  let mut harvest = harvest::HarvestClient::new();
-  harvest.prepare().await?;
+  load_env();
+  let cfg = config::Config::new().unwrap();
+  let mut harvest = harvest::HarvestClient::new(&cfg);
+  if cfg.harvest_user_id == None {
+    harvest.prepare().await?;
+  }
   let entries = harvest.time_entries().await?.time_entries;
-  println!("{:?}", entries);
-
-  entries.iter().for_each(|e| {
-    println!(
-      "{when},{duration},{what},{project}",
-      when = e.spent_date,
-      duration = e.hours,
-      what = format!("{} - {}", e.task.name, e.notes),
-      project = e.project.name
-    )
-  });
-
-  println!("{}", entries.iter().map(|e| e.hours).sum::<f64>());
+  let result = template::render_file_template(entries, &cfg.template_path.unwrap()).unwrap();
+  println!("{}", result);
 
   Ok(())
+}
+
+fn load_env() {
+  match dotenv::dotenv() {
+    Ok(path) => println!(
+      "Loaded environmental variables from path: {}",
+      path.to_string_lossy()
+    ),
+    Err(_) => print!("Ignoring loading .env file, as it was not found..."),
+  }
 }
