0a8b27382ababd29da0e47b84f5355d355acbaf0
Arkadiusz Żmudzin <arek@moodup.team>
2020-08-03 03:24:09 +0200

    Use config to initialize HarvestClient

--- src/harvest/mod.rs
+++ src/harvest/mod.rs
@@ -1,26 +1,37 @@
-mod api;
+pub mod api;
 mod credentials;
 
+use super::config::Config;
 use api::query::TimeEntriesQuery;
 use api::TimeEntries;
 use credentials::HarvestCredentials;
 
 use chrono::offset::TimeZone;
 use chrono::Datelike;
-use chrono::Local;
+use chrono::{Local, NaiveDate};
 
 #[derive(Debug)]
 pub struct HarvestClient {
   credentials: HarvestCredentials,
   user_id: Option<u32>,
+  query: QueryOptions,
+}
+
+#[derive(Debug)]
+struct QueryOptions {
+  from: Option<NaiveDate>,
+  to: Option<NaiveDate>,
 }
 
 impl HarvestClient {
-  pub fn new() -> HarvestClient {
+  pub fn new(config: &Config) -> HarvestClient {
     HarvestClient {
-      credentials: HarvestCredentials::from_env()
-        .expect("Could not load harvest credentials from envs"),
-      user_id: None,
+      credentials: HarvestCredentials::from_cfg(config),
+      user_id: config.harvest_user_id,
+      query: QueryOptions {
+        from: config.start_date,
+        to: config.end_date,
+      },
     }
   }
 
@@ -34,13 +45,11 @@ impl HarvestClient {
   pub async fn time_entries(&self) -> Result<TimeEntries, reqwest::Error> {
     let today = Local::now().date();
     let query = TimeEntriesQuery {
-      user_id: self.user_id.expect("HarvestClient was not prepared"),
-      from: Some(
-        Local
-          // .ymd(today.year(), today.month(), today.day())
-          .ymd(today.year(), 7, 29)
-          .and_hms(0, 0, 0),
-      ),
+      user_id: self
+        .user_id
+        .expect("No user ID, provide user ID or use `prepare` method"),
+      from: self.query.from,
+      to: self.query.to,
     };
 
     api::time_entries(&query, &self.credentials).await
