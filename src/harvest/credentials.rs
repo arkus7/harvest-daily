55b1e0fc87cd911d01cd2fc2634ccada7f632442
Arkadiusz Żmudzin <arek@moodup.team>
2020-08-03 03:22:31 +0200

    Load harvest credentials from config

--- src/harvest/credentials.rs
+++ src/harvest/credentials.rs
@@ -1,22 +1,23 @@
-use envy;
+use crate::config::Config;
 use serde::Deserialize;
+
 #[derive(Deserialize, Debug)]
 pub struct HarvestCredentials {
   #[serde(rename = "harvest_account_id")]
-  pub account_id: String,
+  pub account_id: u32,
   #[serde(rename = "harvest_token")]
   pub token: String,
 }
 
 impl HarvestCredentials {
-  pub fn from_env() -> Result<HarvestCredentials, envy::Error> {
-    match dotenv::dotenv() {
-      Ok(path) => println!(
-        "Loaded environmental variables from path: {}",
-        path.to_string_lossy()
-      ),
-      Err(_) => print!("Ignoring loading .env file, as it was not found..."),
-    }
-    envy::from_env::<HarvestCredentials>()
+  pub fn from_cfg(config: &Config) -> HarvestCredentials {
+    let token = config
+      .harvest_token
+      .clone()
+      .expect("Harvest: Missing token");
+    let account_id = config
+      .harvest_account_id
+      .expect("Harvest: Missing Account ID");
+    HarvestCredentials { account_id, token }
   }
 }
